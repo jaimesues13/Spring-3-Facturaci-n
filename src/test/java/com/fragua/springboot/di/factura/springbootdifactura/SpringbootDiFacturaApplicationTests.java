e4e7959af3f8ce6c7536745410825b204cda91ac
Jaime Suarez Espinoza <jaime_suarez@fragua.com.mx>
2024-05-02 15:32:54 -0600

    first commit

---
+++ src/test/java/com/fragua/springboot/di/factura/springbootdifactura/SpringbootDiFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fragua.springboot.di.factura.springbootdifactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
